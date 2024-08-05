d26b5371d2f2a0f20b99b0e76f507d9c0cf0f338
Pentyala Bhargava Ram <73393502+bhrgv-rm@users.noreply.github.com>
2024-08-05 13:24:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1>asfa</h1>;
+export default function Home(){
+  return (
+    <div className='main-page'>
+      <h1>Click on the URL and add <span>/(chapter)/(verse)</span> to go to the specific verse that is required.</h1>
+      <p>Example - The URL <a href="https://cerulean-pegasus-129918.netlify.app/10/2">https://cerulean-pegasus-129918.netlify.app/10/2</a> goes to <b>Chapter 10, Verse 2</b></p>
+    </div>
+  )
 }
